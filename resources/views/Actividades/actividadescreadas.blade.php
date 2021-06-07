86522486b14135726bd73fd62f440267270a8fb4
Sebastián Aguilar <al221810693@gmail.com>
2021-06-06 20:32:34 -0500

    Cambio en el controlador y vista de actividades

--- resources/views/Actividades/actividadescreadas.blade.php
+++ resources/views/Actividades/actividadescreadas.blade.php
@@ -33,14 +33,15 @@
         	filter
         	data = "{{$json}}">
         	<zg-colgroup>
-            	<zg-column index='turno' header='Turno' width="100" type='text'></zg-column>
-            	<zg-column index='asunto' header='Asunto' width="100" type='text'></zg-column>
-            	<zg-column index='descripcion' header='Descripcion' width="200" type='text'></zg-column>
-            	<zg-column index='fecha_creacion' header='Fecha de creacion' width="100" type='text'></zg-column>
-            	<zg-column index='comunicado' header='Comunicado' width="200" type='text'></zg-column>
-            	<zg-column index='fecha_inicio' header='Fecha de inicio' width="100" type='text'></zg-column>
-            	<zg-column index='importancia' header='Importancia' width="100" type='text'></zg-column>
-            	<zg-column index='status' header='Estatus' width="100" type='text'></zg-column>
+            	<zg-column index='turno' header='Turno' width="100" type='number'></zg-column>
+                    <zg-column index='fecha_creacion' header='Fecha creación' width="120" type='text'></zg-column>
+                    <zg-column index='creador' header='Creador' width="120" type='text'></zg-column>
+                    <zg-column index='periodo' header='Periodo' width="120" type='text'></zg-column>
+                    <zg-column index='importancia' header='Importancia' width="120" type='text'></zg-column>
+                    <zg-column index='nombre' header='Area responsable' width="120" type='text'></zg-column>
+                    <zg-column index='C' header='Acance' width="100" type='text'></zg-column>
+                    <zg-column index='AB' header='Atendido por' width="100" type='text'></zg-column>
+                    <zg-column align="center" filter ="disabled" index='operaciones' header='Operaciones' width="120" type='text'></zg-column>
         	</zg-colgroup>
     	</zing-grid>
 	</div>
