b3ad0b21e0804f38d2f99c457e45ce4d5faa5dc6
Uriel Aguilar Ortega <al221711149@gmail.com>
2021-06-08 20:50:10 -0500

    Cambios de uriel

--- app/Http/Controllers/SeguimientoController.php
+++ app/Http/Controllers/SeguimientoController.php
@@ -9,6 +9,8 @@ use App\Models\archivosSeguimientos;
 use App\Models\responsablesActividades;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Session;
 
 class SeguimientoController extends Controller
@@ -16,72 +18,91 @@ class SeguimientoController extends Controller
 
     public function actividades_asignadas()
     {
-        $consult = DB::table('responsables_actividades')
-            ->join('actividades', 'actividades.idac', '=', 'responsables_actividades.idac_actividades')
-            ->join('users', 'users.idu', '=', 'responsables_actividades.idu_users')
-            ->select(
-                'actividades.idac',
-                'actividades.asunto',
-                'actividades.descripcion',
-                'actividades.fecha_creacion',
-                'actividades.turno',
-                'actividades.comunicado',
-                'actividades.fecha_inicio',
-                'actividades.fecha_fin',
-                'actividades.hora_inicio',
-                'actividades.hora_fin',
-                'actividades.status',
-                'actividades.importancia',
-                'users.idu',
-                'users.titulo',
-                'users.nombre',
-                'users.app',
-                'users.apm',
+        $consult = DB::SELECT("SELECT  ac.idac ,ac.turno, ac.fecha_creacion, ac.asunto ,CONCAT(us.titulo, ' ', us.nombre, ' ', us.app, ' ', us.apm) AS creador, 
+        ac.fecha_inicio, ac.fecha_fin, ac.importancia, ar.nombre as area, ra.acuse, ra.idu_users, 
+        porcentaje(ac.idac) AS porcentaje
+        FROM actividades AS ac
+        INNER JOIN users AS us ON us.idu = ac.idu_users
+        INNER JOIN areas AS ar ON ar.idar = ac.idar_areas
+        LEFT JOIN responsables_actividades AS ra ON ra.idac_actividades = ac.idac
+        LEFT JOIN seguimientos_actividades AS sa ON sa.idreac_responsables_actividades = idreac
+        GROUP BY ac.idac
+        ORDER BY ac.fecha_creacion DESC");
 
-            )
-            ->where('users.idu', '=', Auth()->user()->idu)
-            ->get();
+        $array = array();
 
+        function recorrer($value){
+            $arr = (gettype($value) == "string") ? explode('-', $value) : null;
+            return $arr;
+        }
+
+        function AB($data)
+        {
+            if (gettype($data) == "array") {
+
+                return $data[0] . " de " . $data[1];
+            } else {
+                return 0;
+            }
+        }
+
+        function C($data)
+        {
+            if (gettype($data) == "array") {
+
+                return number_format($data[2], 2, '.', ' ') . '%';
+            } else {
+                return 0.00;
+            }
+        }
+
+        function ver($idac)
+        {
+            return "<a class='btn btn-success mt-1 btn-sm' href=" . route('Seguimiento', ['idac' => encrypt($idac)]) . "><i class='nav-icon fas fa-eye'></i></a>";
+        }
+
+        foreach ($consult as $c) {
+
+            $data = recorrer($c->porcentaje);
+
+            array_push($array, array(
+
+                'turno' => $c->turno,
+                'fecha_creacion' => Carbon::parse($c->fecha_creacion)->locale('es')->isoFormat('D MMMM h:mm a'),
+                'asunto' => $c->asunto,
+                'creador' => $c->creador,
+                'periodo' => Carbon::parse($c->fecha_inicio)->locale('es')->isoFormat('D MMMM') . ' - ' . Carbon::parse($c->fecha_fin)->locale('es')->isoFormat('D MMMM'),
+                'importancia' => $c->importancia,
+                'area' => $c->area,
+                'recibo' => AB($data),
+                'porcentaje' =>  C($data),
+                'operaciones' => ver($c->idac),
+            ));
+        }
+
+        $json = json_encode($array);
 
         return view('SeguimientoActividades.actividades_asignadas')
-            ->with('consult', $consult);
+            ->with('json', $json);
     }
 
     public function Seguimiento($idac)
     {
-        //Encriptar el id de la actividad que se esta consulutando
+        //Encriptar el id de la actividad que se esta consulutando 
         $idac = decrypt($idac);
+
         //Obtener detalles de la actividad
-        $actividades = DB::table('actividades')
-            ->join('users', 'users.idu', '=', 'actividades.idu_users')
-            ->join('areas', 'areas.idar', '=', 'actividades.idar_areas')
-            ->select(
-                'actividades.idac',
-                'actividades.asunto',
-                'actividades.descripcion',
-                'actividades.fecha_creacion',
-                'actividades.turno',
-                'actividades.comunicado',
-                'actividades.fecha_inicio',
-                'actividades.fecha_fin',
-                'actividades.hora_inicio',
-                'actividades.hora_fin',
-                'areas.nombre as nombre_area',
-                'users.titulo',
-                'users.nombre',
-                'users.app',
-                'users.apm',
-                'actividades.status',
-                'actividades.importancia',
-                'actividades.archivo1',
-                'actividades.archivo2',
-                'actividades.archivo3',
-                'actividades.link1',
-                'actividades.link2',
-                'actividades.link3',
-            )
-            ->where('idac', $idac)
-            ->get();
+
+        $actividades = DB::SELECT("SELECT  ac.idac ,ac.turno, ac.fecha_creacion, ac.asunto, ac.descripcion,
+        CONCAT(us.titulo, ' ', us.nombre, ' ', us.app, ' ', us.apm) AS creador, ac.comunicado,
+        ac.fecha_inicio, ac.fecha_fin, ac.importancia, ar.nombre as nombre_area, 
+        ac.archivo1, ac.archivo2, ac.archivo3, ac.link1, ac.link2, ac.link3, 
+        ac.status, porcentaje(ac.idac) AS porcentaje
+        FROM actividades AS ac
+        INNER JOIN users AS us ON us.idu = ac.idu_users
+        INNER JOIN areas AS ar ON ar.idar = ac.idar_areas
+        WHERE ac.idac = $idac");
+
 
         //Obtener datos del usuario
         $user = DB::table('users')
@@ -101,7 +122,7 @@ class SeguimientoController extends Controller
             ->where('users.idu', '=', Auth()->user()->idu)
             ->get();
 
-        //Obtener la fecha actual
+        //Obtener la fecha actual 
         $now = Carbon::now();
 
         //Obtener el responsable que le esta dando seguimiento ala actividad
@@ -111,13 +132,23 @@ class SeguimientoController extends Controller
                 'responsables_actividades.idreac',
                 'responsables_actividades.idu_users',
                 'responsables_actividades.idac_actividades',
+                'responsables_actividades.acuse',
 
             )
             ->where('responsables_actividades.idu_users', '=', Auth()->user()->idu)
             ->where('idac', $idac)
             ->get();
 
-        //dd($resp);
+        //Marca que el usuario le ha empezado a dar seguimiento a su actividad en el campo acuse = 1
+
+        $consulta = $resp[0]->idreac;
+        $acuse = $resp[0]->acuse;
+        if ($acuse == '0') {
+            DB::UPDATE("UPDATE responsables_actividades SET acuse = '1' WHERE idreac = $consulta");
+        }
+
+
+
 
         //Obtener los seguimientos que se le ha dado a la actividad asignada
         $seguimientos = DB::table('seguimientos_actividades')
@@ -129,43 +160,55 @@ class SeguimientoController extends Controller
                 'seguimientos_actividades.estado',
                 'seguimientos_actividades.porcentaje',
                 'responsables_actividades.idu_users',
+                'responsables_actividades.idac_actividades',
+                'seguimientos_actividades.idreac_responsables_actividades',
             )
+            ->where('responsables_actividades.idac_actividades', '=', $idac)
             ->where('responsables_actividades.idu_users', '=', Auth()->user()->idu)
-
             ->get();
+
+        $array_sa = array();
+
+        function detalles()
+        {
+            return "<a class='btn btn-success mt-1 btn-sm' href=''><i class='nav-icon fas fa-eye'></i></a>
+                        <a class='btn btn-danger mt-1 btn-sm' href=''><i class='nav-icon fas fa-trash'></i></a>";
+        }
+
+        foreach ($seguimientos as $seg_ac) {
+
+            array_push($array_sa, array(
+
+                'idseac' => $seg_ac->idseac,
+                'fecha' => Carbon::parse($seg_ac->fecha)->locale('es')->isoFormat('D MMMM h:mm a'),
+                'detalle' => $seg_ac->detalle,
+                'estado' => $seg_ac->estado,
+                'porcentaje' => $seg_ac->porcentaje,
+                'evidencia' => detalles(),
+            ));
+        }
+
+        $json_sa = json_encode($array_sa);
+
 
         return view('SeguimientoActividades.Seguimiento')
             ->with('actividades', $actividades[0])
             ->with('resp', $resp[0])
-            ->with('seguimientos', $seguimientos)
+            ->with('json_sa', $json_sa)
             ->with('user', $user[0])
             ->with('now', $now);
     }
 
     public function AgregarSeguimiento(Request $request)
     {
-
         $idseac = $request->idseac;
         $idreac_responsables_actividades = $request->idreac_responsables_actividades;
+        $idseac_seguimientos_actividades = $request->idseac_seguimientos_actividades;
         $now = Carbon::now();
         $detalle = $request->detalle;
         $porcentaje = $request->porcentaje;
         $estado = $request->estado;
         $ruta = $request->ruta;
-
-        if (\Storage::disk('local')->exists($request->archivosoculto)) {
-
-            $archivos = $request->archivosoculto;
-        } elseif ($request->file('ruta') != null) {
-
-            $file = $request->file('ruta');
-            $archivos = $file->getClientOriginalName();
-            $archivos = date('Ymd_His_') . $archivos;
-            \Storage::disk('local')->put($archivos, \File::get($file));
-        } else {
-
-            $archivos = 'Sin archivo';
-        }
 
         $seg_ac = new seguimientosActividades;
         $seg_ac->idreac_responsables_actividades = $idreac_responsables_actividades;
@@ -176,17 +219,27 @@ class SeguimientoController extends Controller
 
         $seg_ac->save();
 
-        $detalle = $request->detalle;
+        //Insertar archivos en tabla archivos_seguimientos
+
+        $max_size = (int) ini_get('upload_max_filesize') * 10240;
+        $user_id = Auth()->user()->idu;
+        $files = $request->file('ruta');
+
+        if ($request->hasFile('ruta')) { }
+
+        foreach ($files as $file) {
+            if (Storage::putFileAs('/Seguimientos/', $file, $file->getClientOriginalName())) {
+
+                archivosSeguimientos::create([
+                    'idseac_seguimientos_actividades' => $idseac_seguimientos_actividades = $seg_ac->idseac,
+                    'nombre' => $file->getClientOriginalName(),
+                    'ruta' => $file->getClientOriginalName(),
+                    'detalle' => $detalle,
+                ]);
+            }
+        }
 
 
-        $seg_arch = new archivosSeguimientos;
-        $seg_arch->ruta =  $archivos;
-        $seg_arch->idseac_seguimientos_actividades =  $seg_ac->idseac;
-        $seg_arch->nombre = $archivos;
-        $seg_arch->detalle = $request->detalle;
-
-
-        $seg_arch->save();
 
         $consid = responsablesActividades::find($seg_ac->idreac_responsables_actividades);
 
--- resources/views/SeguimientoActividades/Seguimiento.blade.php
+++ resources/views/SeguimientoActividades/Seguimiento.blade.php
@@ -1,32 +1,59 @@
 @extends('layout.layout')
 @section('content')
+
+@section('header')
+
+<script src='{{asset('src/js/zinggrid.min.js')}}'></script>
+<script src='{{asset('src/js/zinggrid-es.js')}}'></script>
+<script>
+    if (es) ZingGrid.registerLanguage(es, 'custom');
+</script>
+
+@endsection
+
 <input type="hidden" value="{{$actividades->idac}}" name="idac">
 
 <div class="row">
     <div class="col-sm-4">
-        <h3>Detalle del turno: {{$actividades->turno}}</h3>
+        <h3 class="display-6">Detalle del turno: {{$actividades->turno}}</h3>
     </div>
     <div class="col-sm-4">
-        <h3>Comunicado: {{$actividades->comunicado}}</h3>
+        <h3 class="display-6">Comunicado: {{$actividades->comunicado}}</h3>
     </div>
     <div class="col-sm-4">
-        <h3>Oficio: UTVT/SEP/000011</h3>
+        <h3 class="display-6">Oficio: UTVT/SEP/000011</h3>
     </div>
 </div>
 
 <div class="card">
     <div class="card-body">
         <div class="row">
-            <div class="col-sm-12"><br>
+            <div class="col-sm-12">
+                <div class="bd-intro ps-lg-4">
+                    <div class="d-md-flex flex-md-row-reverse align-items-center justify-content-between">
+                        <h1 class="bd-title" id="content">{{$actividades->asunto}}</h1>
+                    </div>
+                    <p class="bd-lead">{{$actividades->descripcion}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-9">
+
+        <div class="card">
+            <div class="card-body">
+
                 <table class="table table-responsive table-striped">
                     <thead class="">
-                        <tr class="bg-primary">
+                        <tr class="bg-secondary">
                             <th scope="col">Turno</th>
-                            <th scope="col">Fecha Creación</th>
-                            <th scope="col">Asunto</th>
+                            <th scope="col">Creación</th>
                             <th scope="col">Creado por </th>
                             <th scope="col">Periodo atención </th>
-                            <th scope="col">Nivel atención </th>
+
                             <th scope="col">Área Responsable</th>
 
                         </tr>
@@ -34,11 +61,13 @@
                     <tbody>
                         <tr>
                             <th scope="row">{{$actividades->turno}}</th>
-                            <td>{{$actividades->fecha_creacion}}</td>
-                            <td>{{$actividades->asunto}}</td>
-                            <td>{{$actividades->titulo}} {{$actividades->nombre}} {{$actividades->app}} {{$actividades->apm}}</td>
-                            <td>{{$actividades->fecha_inicio}} - al - {{$actividades->fecha_fin}}</td>
-                            <td>{{$actividades->importancia}}</td>
+
+                            <td> {{ Carbon\Carbon::parse($actividades->fecha_creacion)->locale('es')->isoFormat('D MMMM') }}</td>
+                            <td>{{$actividades->creador}} </td>
+                            <td>
+                                {{ Carbon\Carbon::parse($actividades->fecha_inicio)->locale('es')->isoFormat('D MMMM') }} al
+                                {{ Carbon\Carbon::parse($actividades->fecha_fin)->locale('es')->isoFormat('D MMMM') }}
+                            </td>
                             <td>{{$actividades->nombre_area}}</td>
                         </tr>
                     </tbody>
@@ -46,29 +75,47 @@
 
                 <table class="table table-responsive table-striped">
                     <thead class="">
-                        <tr class="bg-primary">
-                            <th scope="col">Avance</th>
+                        <tr class="bg-secondary">
                             <th scope="col">Atendido por</th>
                             <th scope="col">Nombre atendió</th>
                             <th scope="col">Cargo</th>
-                            <th scope="col">Avance indv.</th>
-                            <th scope="col">Status Atención</th>
                             <th scope="col">Acuse Recibido</th>
+                            <th scope="col">Nivel atención </th>
 
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row">80%</th>
-                            <td>3 de 3</td>
+                            <td>2</td>
                             <td>{{Auth()->user()->titulo . ' ' . Auth()->user()->nombre . ' '  .Auth()->user()->app . ' ' . Auth()->user()->apm}}</td>
                             <td>{{$user->tipo_usuario . ' - ' . $user->nombre_areas}}</td>
-                            <td>100%</td>
-                            <td>En tiempo</td>
                             <td>Si</td>
+                            <td>{{$actividades->importancia}}</td>
+
                         </tr>
                     </tbody>
                 </table>
+            </div>
+
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="bd-intro ps-lg-4">
+                    <div class="d-md-flex align-items-center justify-content-between">
+                        <h3 class="bd-title">Avance</h3>
+                    </div>
+                    <p class="bd-lead">General</p>
+                    <p class="bd-lead">Individual</p>
+                    <div class="d-md-flex align-items-center justify-content-between">
+                        <h3 class="bd-title">Status atención</h3>
+                    </div>
+                    <p class="bd-lead">En tiempo</p>
+
+
+                </div>
+
             </div>
         </div>
     </div>
@@ -138,8 +185,8 @@
 
                     <div class="col-sm-10">
 
-                        <label for="formFileSm" class="form-label">Seleccione Archivo</label>
-                        <input class="form-group form-group-sm" id="formFileSm" name="ruta" type="file" multiple>
+                        <label for="ruta[]" class="form-label">Seleccione Archivo</label>
+                        <input class="form-group form-group-sm" id="ruta[]" name="ruta[]" type="file" multiple>
 
                     </div>
 
@@ -152,70 +199,26 @@
                     </div>
                     <div class="col-sm-2">
 
-                        <button class="btn btn-sm btn-success">+</button>
+                        <button class="btn btn-sm btn-success">Guardar seguimiento</button>
 
                     </div>
 
                 </form>
-
-
-
             </div>
         </div>
     </div>
 
     <div class="col-sm-7">
-        <div class="card">
-            <div class="card-body">
-                <center>
-                    <h4>Seguimientos de la actividad</h4>
-                </center><br>
-                @if (Session::has('message'))
-                <p class="alert alert-info">
-                    {{Session::get('message')}}
-                </p>
-                @endif
-                @if (Session::has('message2'))
-                <p class="alert alert-danger">
-                    {{Session::get('message2')}}
-                </p>
-                @endif
-                <table class="table table-responsive">
-                    <thead class="">
-                        <tr class="bg-primary">
-                            <th scope="col">No. Segui.</th>
-                            <th scope="col">Fecha avance</th>
-                            <th scope="col">Detalle</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">% Avance</th>
-                            <th scope="col">Archivos Evidencia</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($seguimientos as $seg)
-                        <tr>
-                            <td>{{$seg->idseac}}</td>
-                            <td>{{$seg->fecha}}</td>
-                            <td>{{$seg->detalle}}</td>
-                            <td>{{$seg->estado}}</td>
-                            <td>{{$seg->porcentaje}}</td>
-                            <td><a href="" class="btn btn-danger"><i class="fa fa-trash"></i></a></td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
 
-            </div>
-        </div>
 
         <div class="card">
             <div class="card-body">
                 <center>
                     <h4>Archivos de la actividad</h4>
                 </center><br>
-                <table class="table table-responsive table-striped">
+                <table class="table table-responsive">
                     <thead class="">
-                        <tr class="bg-primary">
+                        <tr class="bg-secondary">
                             <th scope="col">Nombre archivo</th>
                             <th scope="col">Detalle</th>
                             <th scope="col">Archivo</th>
@@ -231,14 +234,14 @@
                         <tr>
                             <td>{{$actividades->archivo1}}</td>
                             <td>{{$actividades->link1}}</td>
-                            <td><a download href="{{asset('archivos/').'/'.$actividades->archivo1}}" class="btn btn-danger"><i class="fa fa-file"></i></a></td>
+                            <td><a download href="{{asset('archivos/').'/'.$actividades->archivo1}}" class="btn btn-sm btn-danger"><i class="fa fa-download"></i></a></td>
                         </tr>
                         @endif
                         @if ($actividades->archivo2 != "Sin archivo")
                         <tr>
                             <td>{{$actividades->archivo2}}</td>
                             <td>{{$actividades->link2}}</td>
-                            <td><a download="" href="{{asset('archivos/').'/'.$actividades->archivo2}}" class="btn btn-danger"><i class="fa fa-file"></i></a></td>
+                            <td><a download="" href="{{asset('archivos/').'/'.$actividades->archivo2}}" class="btn btn-sm btn-danger"><i class="fa fa-download"></i></a></td>
                         </tr>
                         @endif
 
@@ -246,7 +249,7 @@
                         <tr>
                             <td>{{$actividades->archivo1}}</td>
                             <td>{{$actividades->link3}}</td>
-                            <td><a download="{{$actividades->archivo3}}" href="{{asset('archivos/').'/'.$actividades->archivo3}}" class="btn btn-danger"><i class="fa fa-file"></i></a></td>
+                            <td><a download="{{$actividades->archivo3}}" href="{{asset('archivos/').'/'.$actividades->archivo3}}" class="btn btn-sm btn-danger"><i class="fa fa-download"></i></a></td>
                         </tr>
                         @endif
                     </tbody>
@@ -255,8 +258,38 @@
             </div>
         </div>
     </div>
+    <div class="col-sm-12">
+        <div class="card">
+            <div class="card-body">
+                <center>
+                    <h4>Seguimientos de la actividad</h4>
+                </center><br>
+                @if (Session::has('message'))
+                <p class="alert alert-info">
+                    {{Session::get('message')}}
+                </p>
+                @endif
+                @if (Session::has('message2'))
+                <p class="alert alert-danger">
+                    {{Session::get('message2')}}
+                </p>
+                @endif
+                <zing-grid lang="custom" caption='Reporte de oficios' sort search pager page-size='3' page-size-options='1,3,5,10' layout='row' viewport-stop theme='android' id='zing-grid' filter data="{{$json_sa}}">
+                    <zg-colgroup>
+                        <zg-column index='idseac' header='No- Seguimiento' width="100" type='text'></zg-column>
+                        <zg-column index='detalle' header='Detalle' width="200" type='text'></zg-column>
+                        <zg-column index='fecha' header='Fecha de avance' width="200" type='text'></zg-column>
+                        <zg-column index='estado' header='Status' width="200" type='text'></zg-column>
+                        <zg-column index='porcentaje' header='% Avance' width="150" type='text'></zg-column>
+                        <zg-column index='evidencia' filter="disabled" header='Importancia' width="150" type='text'></zg-column>
+                    </zg-colgroup>
+                </zing-grid>
+
+            </div>
+        </div>
+    </div>
 
 </div>
 
 
-@stop
+    @stop
