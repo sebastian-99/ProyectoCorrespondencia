c14041f155605a9d8561a6533a792010edd20482
Ismael <ismaelgarciam98@gmail.com>
2021-06-22 00:26:03 -0500

    Avance de pdf

--- resources/views/Actividades/pdf.blade.php
+++ resources/views/Actividades/pdf.blade.php
@@ -1,57 +1,80 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PDF de actividades seguimientos</title>
+<!DOCTYPE html> 
+<html lang="es"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+    <title>PDF de acuse recibido de actividades</title> 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="{{public_path('src/css/pdf.css')}}">
 </head>
 <style>
-    *{
-        padding: 0;
-        font-family: arial;
-    }
     h1 {
+        font-family: Arial;
         font-size: 25px;
         text-align: center;
         color: #0B821D;
     }
     h2 {
+        font-family: Arial;
         font-size: 22px;
         text-align: center;
     }
-    p {
-        text-align: center;
+    div p {
+        font-family: Arial;
+        font-size: 16px;
+        text-align: left;
+        line-height: 1.5em;
     }
+
+    footer {
+        position: fixed;
+        bottom: 0cm;
+        height: 2cm;
+        margin-left: 0px;
+        margin-right: 0px;
+        width: 100%;
+        background-color: #696969;
+        color: white;
+        font-family: Arial;
+        font-size: 8pt;
+        /*line-height: 35pt;*/
+    }
+    div img{
+        left: 0px;
+        right: 0px;
+        margin-left: 0px;
+    }
+    
 </style>
-
-
 <body>
     <div>
-        <div class="row">
-            <div align="left"><img src="{{public_path('images/Edomex.png')}}" height="60" alt=""></div>
-            <div align="right"><img src="{{public_path('images/logoUTVT.png')}}" height="90" alt=""></div>
+        <div align="right">
+            <div>
+                <img src="{{public_path('images/logoGob.png')}}" height="35" align="left">
+                <img src="{{public_path('images/logoUTVT.png')}}" height="35" alt="">
+                <img src="{{public_path('images/Edomex.png')}}" height="35" alt="">
+            </div>
         </div>
     </div>
-    <h1>REPORTE DE ACUSE DE RECIBIDO</h1><br>
+    <br><br>
+    <center><p>“2021. Año de la Consumación de la Independencia y la Grandeza de México".</p></center>
+    <h1 style="font-family: Arial;">REPORTE DE ACUSE DE RECIBIDO</h1><br>
     <h2>DETALLE DE LA ACTIVIDAD</h2>
 
     <table class="table table-bordered border-primary">
         <tr>
-            <td scope="row" width='50%'>
-                <div class="row">
-                    <div class="col">
+            <td width='50%'>
+                <div>
+                    <div class="col" style="font-family: Arial;">
                         <p>Folio: <b>{{$data[0]->comunicado}}</b></p>
                         <p>Asunto: <b>{{$data[0]->asunto}}</b></p>
                         <p>Detalle: <b>{{$data[0]->descripcion}}</b></p>
                     </div>
                 </div>
             </td>
-            <td scope="row" width='50%'>
-                <div class="row">
-                    <div class="col">
+            <td width='50%'>
+                <div>
+                    <div>
                         <p>Fecha creaci&oacute;n: <b>{{$data[0]->fecha_creacion}}</b></p>
                         <p>Fecha seguimiento: <b>{{$data[0]->fecha_inicio}}</b></p>
                         <p>Fecha termino: <b>{{$data[0]->fecha_fin}}</b></p>
@@ -94,16 +117,16 @@
                 {
                     $contenido .= "<td scope='row' width='30%'> <div class='row'> <div class='col'>";
 
-                    $contenido .= "<p>Nombre: ".$insert[$i]['nombre']."</p>";
+                    $contenido .= "<p>Nombre: <b>".$insert[$i]['nombre']."</b></p>";
 
-                    $contenido .= "<p>Fecha: ".$insert[$i]['fecha_acuse']."</p>";
+                    $contenido .= "<p>Fecha: <b>".$insert[$i]['fecha_acuse']."</b></p>";
 
                     //$contenido .= "<p>Razón: FIRMA DE RECIBIDO</p>";
 
-                    $contenido .= "<p>Área: ".$insert[$i]['area']."</p>";
+                    $contenido .= "<p>Área: <b>".$insert[$i]['area']."</b></p>";
 
-                    $contenido .= "<p>Firma: ".$insert[$i]['firma']."</p>";
-                    $contenido .= "<p>".$insert[$i]['firma2']."<p>";
+                    $contenido .= "<p>Firma: <b>".$insert[$i]['firma']."</b></p>";
+                    $contenido .= "<p><b>".$insert[$i]['firma2']."</b><p>";
 
                     $contenido .= "</div> </div> </td>";
 
@@ -138,16 +161,16 @@
             {
                 $contenido .= "<td scope='row' width='30%'> <div class='row'> <div class='col'>";
 
-                $contenido .= "<p>Nombre: ".$out[$i]['nombre']."</p>";
+                $contenido .= "<p>Nombre: <b>".$out[$i]['nombre']."</b></p>";
 
-                $contenido .= "<p>Fecha: ".$out[$i]['fecha_acuse']."</p>";
+                $contenido .= "<p>Fecha: <b>".$out[$i]['fecha_acuse']."</b></p>";
 
                 //$contenido .= "<p>Razón: FIRMA DE RECIBIDO</p>";
 
-                $contenido .= "<p>Área: ".$out[$i]['area']."</p>";
+                $contenido .= "<p>Área: <b>".$out[$i]['area']."</b></p>";
 
-                $contenido .= "<p>Firma: ".$out[$i]['firma']."</p>";
-                $contenido .= "<p>".$insert[$i]['firma2']."<p>";
+                $contenido .= "<p>Firma: <b>".$out[$i]['firma']."</b></p>";
+                $contenido .= "<p><b>".$insert[$i]['firma2']."</b><p>";
 
                 $contenido .= "</div> </div> </td>";
 
@@ -160,7 +183,14 @@
 
     @endphp
                  
-    </table>
+    </table>    
     
+    
+
+    <footer>
+        <div><img src="{{public_path('images/M-Edomex.png')}}" width="170" height="170" align="left"></div>
+        <br><br>
+        <center><p>Carretera del Departamento del D.F. km 7.5, Santa María Atarasquillo, C.P. 52044, Lerma, Estado de México. <br>Tels.: (728) 285 95 52, 285 99 69, 282 22 47. utvtol.edu.mx</p></center>
+    </footer>
 </body>
 </html>
