cc0ff692993b687611e7f921c77f21bd6dbe2452
Mariana <kos.k8.MC@gmail.com>
2021-06-24 18:07:46 -0500

    leyenda_O

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -8,12 +8,12 @@
     <b>Correo Electrónico:</b>
     <input type="hidden" name="token" value="{{ $token }}">
     <div class="form-group">
-    <input type="text" class="form-control rounded-left"  class="@error('email') is-invalid @enderror" placeholder="Correo electrónico" name="email" value="{{email}}" >
+    <input type="text" class="form-control rounded-left"  class="@error('email') is-invalid @enderror" placeholder="Correo electrónico" name="email" value="{{$email}}" >
+    <i style="color:red;">Es obligatorio colocar tu correo </i>
 	@error('email')
     <div class="form-text text-danger">{{ $message }}</div>
 	@enderror
 	</div>
-
     <b>Nueva Contraseña:</b>
     <div class="form-group">
 		<input type="password" class="form-control rounded-left"  class="@error('password') is-invalid @enderror" placeholder="Contraseña" name="password"  >
@@ -32,12 +32,6 @@
     <br>
     <br>
     <br>
-    <p style="background: #bdf0fa; color: #0c92ac; font-weight: bold; padding: 15px; border: 2px solid #abecf9; border-radius: 6px;">
-    INSTRUCCIONES:<br>Tu contraseña debe de contener 8 caracteres minimo:
-    <br>-Una mayuscula
-    <br>-Números.
-    <br>-Minúsculas
-    <br>-Un Simbolo</p>
-
+    
 </form>
 @endsection
