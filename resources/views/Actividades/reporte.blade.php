635a46b1d34d565d5af90931d0dd515dc1c81195
AlyUK <al221712237@gmail.com>
2021-06-26 19:07:07 -0500

    Acentos

--- resources/views/Actividades/reporte.blade.php
+++ resources/views/Actividades/reporte.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.layout')
 @section('content')
 @section('header')
-    
-    <script src='{{asset('src/js/zinggrid.min.js')}}'></script>    
-    <script src='{{asset('src/js/zinggrid-es.js')}}'></script>   
+
+    <script src='{{asset('src/js/zinggrid.min.js')}}'></script>
+    <script src='{{asset('src/js/zinggrid-es.js')}}'></script>
     <script>
       if (es) ZingGrid.registerLanguage(es, 'custom');
-    </script> 
-    
+    </script>
+
 @endsection
 <div class="card">
     <div class="card-header">
@@ -19,7 +19,7 @@
          <a href="{{route('create_actividades')}}"><button class="btn btn-primary">Nuevo</button></a>
         </div>
       </div>
-      
+
     </div>
     <div class="card-body">
     @if (Session::has('message'))
@@ -36,17 +36,17 @@
                 <p class="alert alert-warning">
                     {{Session::get('message3')}}
                 </p>
-                @endif  
+                @endif
               <zing-grid
-              
-                lang="custom" 
-                caption='Reporte de oficios' 
-                sort 
-                search 
-                pager 
-                page-size='10' 
-                page-size-options='1,2,3,4,5,10' 
-                layout='row' 
+
+                lang="custom"
+                caption='Reporte de oficios'
+                sort
+                search
+                pager
+                page-size='10'
+                page-size-options='1,2,3,4,5,10'
+                layout='row'
                 viewport-stop
                 theme='android'
                 id='zing-grid'
@@ -55,12 +55,12 @@
                 <zg-colgroup>
                     <zg-column index='turno' header='Turno'  width="100" type='number'></zg-column>
                     <zg-column index='asunto' header='Asunto' width="200" type='text'></zg-column>
-                    <zg-column index='descripcion' header='Descripcion'  width='200'></zg-column>
+                    <zg-column index='descripcion' header='Descripción'  width='200'></zg-column>
                     <zg-column index='fecha_creacion' header='Fecha creación' width="200" type='text'></zg-column>
                     <zg-column index='creador' header='Creador' width="200" type='text'></zg-column>
                     <zg-column index='periodo' header='Periodo' width="220" type='text'></zg-column>
                     <zg-column index='importancia' header='Importancia' width="130" type='text'></zg-column>
-                    <zg-column index='nombre' header='Area responsable' width="170" type='text'></zg-column>
+                    <zg-column index='nombre' header='Área responsable' width="170" type='text'></zg-column>
                     <zg-column index='C' header='Avance' width="120" type='text'></zg-column>
                     <zg-column index='AB' header='Atendido por' width="135" type='text'></zg-column>
                     <zg-column index='E' header='Estado' width="120" type='text'></zg-column>
@@ -70,5 +70,5 @@
     </div>
   </div>
 
- 
+
 @endsection
