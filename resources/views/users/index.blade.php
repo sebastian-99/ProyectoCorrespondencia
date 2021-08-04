9295c49724f5119747c130bb63fff352d9f4b70d
AlyUK <al221712237@gmail.com>
2021-08-04 17:44:32 -0500

    Algunas correcciones

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -29,7 +29,7 @@
      <div class="card-body">
     	<zing-grid
         	lang="custom"
-        	caption='Reporte de tipos de actividades'
+        	caption='Reporte de usuarios'
         	sort
         	search
         	pager
