ba4c141591dfbf2d253c5342dff760f21aa67e66
mariana <al221811726@gmail.com>
2021-10-05 01:42:02 -0500

    Recordatorio2

--- resources/views/mails/recordatorio.blade.php
+++ resources/views/mails/recordatorio.blade.php
@@ -5,17 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body bgcolor="C0F6B5">
-<div align="center"><img src="{{asset('images/siemprecuervos.png')}}" width="180" height="40"></div>
-<br>
-<br>
-    <div>
-   <center><h1><font color="red">¡Recordatorio!</font></h1></center>
-                  <h2> 'Hola, que tal:'</h2>
-                  <h3>Le recordamos que las actividad que fue asignada está por concluir, se le comunica que pueda realizarla a la brevedad. </h3>
-                 
-                    
-    </div>
+<body style="background-color:#187905">
+    <section class="ftco-section">
+        <div align="center"><img src="{{asset('images/siemprecuervos.png')}}" width="200" height="90">
+        </div>
+            <br>
+            <br>
+                <div class="card-footer">
+                    <div>
+                        <center><h1><font color="White">¡RECORDATORIO!</font></h1></center>
+                            <h2 align=center style="color:#008000"> Hola, que tal:</h2>
+                            <h3 align=centerstyle="color:#008000" >Le recordamos que las actividades que fueron asignadas están por concluir, se le comunica que pueda realizarla a la brevedad. </h3>
+                            <h2 align=center style="color:#008000"> Gracias,  se le agrasece su compromiso.</h2>
+                    </div>
+                </div>
+
+    </section>
 </body>
 </html>
 
