9f7204f2194120b283f54a6e76ffc02ba5bc89aa
mariana <al221811726@gmail.com>
2021-10-05 00:53:22 -0500

    Recordatorio

--- resources/views/mails/recordatorio.blade.php
+++ resources/views/mails/recordatorio.blade.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
-<body>
-    <h1>Te llego un mail paps.</h1>
+<body bgcolor="C0F6B5">
+<div align="center"><img src="{{asset('images/siemprecuervos.png')}}" width="180" height="40"></div>
+<br>
+<br>
+    <div>
+   <center><h1><font color="red">¡Recordatorio!</font></h1></center>
+                  <h2> 'Hola, que tal:'</h2>
+                  <h3>Le recordamos que las actividad que fue asignada está por concluir, se le comunica que pueda realizarla a la brevedad. </h3>
+                 
+                    
+    </div>
 </body>
 </html>
+
